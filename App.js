3bdf17faf32e298d3e5160d31ed13cc66cdf3844
Tóth Ferenc <tothfo@gmail.com>
2019-11-26 19:34:09 +0100

    error fixes

--- App.js
+++ App.js
@@ -1,33 +1,31 @@
-import React, { Component } from 'react';
-import { StyleSheet } from 'react-native';
+import React, {Component} from 'react';
+import {StyleSheet} from 'react-native';
 import ResultsFeed from './src/ResultsFeed';
 import LoginScreen from './src/LoginScreen';
-import { Router, Scene } from 'react-native-router-flux';
+import {Router, Scene} from 'react-native-router-flux';
 import MyMenu from './src/MyMenu';
 import UserData from './src/ResultData';
 
 export default class App extends Component {
-
   render() {
     return (
       <Router
-        navigationBarStyle={ styles.navigation }
-        navBarButtonColor={ 'rgb(255,255,255)' }
-        titleStyle={ styles.textStyle }>
-
-        <Scene key='root'>
+        navigationBarStyle={styles.navigation}
+        navBarButtonColor={'rgb(255,255,255)'}
+        titleStyle={styles.textStyle}>
+        <Scene key="root">
           <Scene
-            key='fokepernyo'
-            component={ LoginScreen }
-            title='Bejelentkezés'
-            hideNavBar='true'
+            key="fokepernyo"
+            component={LoginScreen}
+            title="Bejelentkezés"
+            hideNavBar="true"
             initial
           />
           <Scene
-            key='eredmenyek'
-            component={ UserData }
-            title='Eredmények'
-            renderRightButton={ <MyMenu/> }
+            key="eredmenyek"
+            component={UserData}
+            title="Eredmények"
+            renderRightButton={<MyMenu />}
           />
         </Scene>
       </Router>
@@ -39,9 +37,9 @@ const styles = StyleSheet.create({
   textStyle: {
     color: 'rgb(255,255,255)',
     flex: 1,
-    textAlign: 'center'
+    textAlign: 'center',
   },
   navigation: {
     backgroundColor: 'rgb(0,0,0)',
-  }
-})
+  },
+});
